486a5d51ecefaeb0277745d374bf1a3b963851ff
José Miguel <clasiccode@gmail.com>
2025-06-23 07:45:05 +0200

    Create EqualRule.cs

---
+++ WinForms.Extensions.Validation/Rules/Nums/EqualRule.cs
@@ -0,0 +1,22 @@
+﻿using JoeDevSharp.WinForms.Extensions.Validation.Interfaces;
+
+namespace JoeDevSharp.WinForms.Extensions.Validation.Rules
+{
+    public class EqualRule : IValidationRule<int>
+    {
+        private readonly int _value;
+        private readonly string _message;
+        public EqualRule(int value, string? message = null)
+        {
+            _value = value;
+            _message = message ?? $"Value must be equal to {_value}.";
+        }
+        public ValidationResult Validate(int value)
+        {
+            if (value != _value)
+                return ValidationResult.Fail(_message);
+            return ValidationResult.Success();
+        }
+    }
+    }
+}
