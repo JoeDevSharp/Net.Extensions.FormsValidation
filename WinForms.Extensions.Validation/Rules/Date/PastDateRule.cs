af09b4bbaa531cf229f677d250b6383772403360
José Miguel <clasiccode@gmail.com>
2025-06-23 09:19:12 +0200

    Create PastDateRule.cs

---
+++ WinForms.Extensions.Validation/Rules/Date/PastDateRule.cs
@@ -0,0 +1,19 @@
+﻿using JoeDevSharp.WinForms.Extensions.Validation.Interfaces;
+
+namespace JoeDevSharp.WinForms.Extensions.Validation.Rules
+{
+    public class PastDateRule : IValidationRule<DateTime>
+    {
+        private readonly string _message;
+        public PastDateRule(string? message = null)
+        {
+            _message = message ?? "Date must be in the past.";
+        }
+        public ValidationResult Validate(DateTime value)
+        {
+            if (value >= DateTime.Now)
+                return ValidationResult.Fail(_message);
+            return ValidationResult.Success();
+        }
+    }
+}
