64905bf9b3bd6a827749a96fa013227bae7d00ae
José Miguel <j.ramos@jm-ramos.com>
2025-06-23 10:48:30 +0200

    Update LessOrEqualRule.cs

--- WinForms.Extensions.Validation/Rules/Nums/LessOrEqualRule.cs
+++ WinForms.Extensions.Validation/Rules/Nums/LessOrEqualRule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JoeDevSharp.WinForms.Extensions.Validation.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace JoeDevSharp.WinForms.Extensions.Validation.Rules
 {
-    internal class LessOrEqualRule
+    public class LessOrEqualRule : IValidationRule<int>
     {
+        private readonly int _threshold;
+        private readonly string _message;
+        public LessOrEqualRule(int threshold, string? message = null)
+        {
+            _threshold = threshold;
+            _message = message ?? $"Value must be less than or equal to {_threshold}.";
+        }
+        public ValidationResult Validate(int value)
+        {
+            if (value > _threshold)
+                return ValidationResult.Fail(_message);
+            return ValidationResult.Success();
+        }
     }
 }
