61f5fe1f8e2b3a85a4621fbc8e4a338b855c5416
José Miguel <clasiccode@gmail.com>
2025-06-23 09:19:02 +0200

    Create MinCountRule.cs

---
+++ WinForms.Extensions.Validation/Rules/Collections/MinCountRule.cs
@@ -0,0 +1,21 @@
+﻿using JoeDevSharp.WinForms.Extensions.Validation.Interfaces;
+
+namespace JoeDevSharp.WinForms.Extensions.Validation.Rules
+{
+    public class MinCountRule : IValidationRule<ICollection<object>>
+    {
+        private readonly int _minCount;
+        private readonly string _message;
+        public MinCountRule(int minCount, string? message = null)
+        {
+            _minCount = minCount;
+            _message = message ?? $"Collection must contain at least {_minCount} items.";
+        }
+        public ValidationResult Validate(ICollection<object> value)
+        {
+            if (value == null || value.Count < _minCount)
+                return ValidationResult.Fail(_message);
+            return ValidationResult.Success();
+        }
+    }
+}
