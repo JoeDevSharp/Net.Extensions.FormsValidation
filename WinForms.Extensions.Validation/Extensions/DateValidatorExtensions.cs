8b28c5e4e83a778a6be9a200597a90516d2cfb08
José Miguel <j.ramos@jm-ramos.com>
2025-06-23 14:12:18 +0200

    Create DateValidatorExtensions.cs

---
+++ WinForms.Extensions.Validation/Extensions/DateValidatorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using JoeDevSharp.WinForms.Extensions.Validation.Rules;
+using JoeDevSharp.WinForms.Extensions.Validation.Rules.Date;
+
+namespace JoeDevSharp.WinForms.Extensions.Validation.Extensions
+{
+    public static class DateValidatorExtensions
+    {
+
+        public static Validator<DateTime> DateRange(this Validator<DateTime> validator, DateTime minDate, DateTime maxDate, string? message = null)
+        {
+            return validator.AddRule(new DateRangeRule(minDate, maxDate, message));
+        }
+
+        public static Validator<DateTime> FutureDateRule(this Validator<DateTime> validator, string? message = null)
+        {
+            return validator.AddRule(new FutureDateRule(message));
+        }
+    }
+}
