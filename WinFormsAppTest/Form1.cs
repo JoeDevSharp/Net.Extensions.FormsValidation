dc9a34cfb02db00b84e1aa5ff4aa5a06568b4366
José Miguel <j.ramos@jm-ramos.com>
2025-06-23 10:43:59 +0200

    Update Form1.cs

--- WinFormsAppTest/Form1.cs
+++ WinFormsAppTest/Form1.cs
@@ -1,6 +1,5 @@
 using JoeDevSharp.WinForms.Extensions.Validation;
 using JoeDevSharp.WinForms.Extensions.Validation.Extensions;
-using JoeDevSharp.WinForms.Extensions.Validation.Rules;
 
 namespace WinFormsAppTest
 {
@@ -12,34 +11,18 @@ namespace WinFormsAppTest
             InitializeComponent();
 
             // Asociamos validadores a controles sin pasar getter explícito
-            _formValidator.AddFieldValidator(textBox1,
-                new Validator<string>()
-                    .AddRule(new RequiredRule<string>("El nombre es obligatorio"))
-                    .AddRule(new LengthRule(min: 3, max: 20)));
 
-            _formValidator.AddFieldValidator(textBox1,
+            _formValidator.AddFieldValidator(
+                "Name",
+                () => textBox1.Text,
                 new Validator<string>()
                     .NotContains("test")
                     .MaxLength(10, "El nombre no puede exceder los 10 caracteres"));
-
-            _formValidator.AddFieldValidator(textBox2,
-                new Validator<string>()
-                    .AddRule(new RequiredRule<string>("El apellido es obligatorio"))
-                    .AddRule(new LengthRule(min: 3, max: 20)));
-
-            _formValidator.AddFieldValidator(textBox3,
-                new Validator<string>()
-                    .AddRule(new RequiredRule<string>("El email es obligatorio"))
-                    .AddRule(new RegexRule(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", "Email no válido")));
-
-            _formValidator.AddFieldValidator(checkBox1,
-                new Validator<bool>()
-                    .AddRule(new CustomRule<bool>(b => b == true, "Debe estar activo")));
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var result = _formValidator.ValidateForm();
+            var result = _formValidator.Validate();
 
             if (!result.IsValid)
             {
