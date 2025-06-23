9c39b8d313c9fe74ca68b42e042806151f328d4e
José Miguel <clasiccode@gmail.com>
2025-06-23 09:18:53 +0200

    Create MustBeTrueRule.cs

---
+++ WinForms.Extensions.Validation/Rules/Bool/MustBeTrueRule.cs
@@ -0,0 +1,19 @@
+﻿using JoeDevSharp.WinForms.Extensions.Validation.Interfaces;
+
+namespace JoeDevSharp.WinForms.Extensions.Validation.Rules
+{
+    public class MustBeTrueRule : IValidationRule<bool>
+    {
+        private readonly string _message;
+        public MustBeTrueRule(string? message = null)
+        {
+            _message = message ?? "Value must be true.";
+        }
+        public ValidationResult Validate(bool value)
+        {
+            if (!value)
+                return ValidationResult.Fail(_message);
+            return ValidationResult.Success();
+        }
+    }
+}
